ea7a922ee61b41915fc255d6641f4c130d3cbfb2
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-24 01:45:48 -0300

    feat: converting the data to the Brazilian standard

--- weather-forecast/src/components/WeatherInfo/WeatherInfo.tsx
+++ weather-forecast/src/components/WeatherInfo/WeatherInfo.tsx
@@ -2,35 +2,55 @@ import React from 'react';
 import './weather.css';
 
 interface WeatherInfoProps {
-  weather: {
-    city: string;
-    date: string;
-    temp: number;
-    tempMax: number;
-    tempMin: number;
-    description: string;
-    icon: string;
-  } | null;
+    weather: {
+        city: string;
+        date: string;
+        temp: number;
+        tempMax: number;
+        tempMin: number;
+        description: string;
+        icon: string;
+    } | null;
 }
 
 const WeatherInfo: React.FC<WeatherInfoProps> = ({ weather }) => {
-  if (!weather) return null;
+    if (!weather) return null;
 
-  return (
-    <div className="weather-info">
-      <header className="weather-header">
-        <h2>{weather.city}</h2>
-      </header>
-      <main className="weather-content">
-        <p>Date: {weather.date}</p>
-        <p>Temperature: {weather.temp}°C</p>
-        <p>Max Temperature: {weather.tempMax}°C</p>
-        <p>Min Temperature: {weather.tempMin}°C</p>
-        <p>Description: {weather.description}</p>
-        <img src={`http://openweathermap.org/img/wn/${weather.icon}.png`} alt={weather.description} />
-      </main>
-    </div>
-  );
+    const weatherDescriptions: { [key: string]: string } = {
+        'clear sky': 'Céu limpo',
+        'few clouds': 'Poucas nuvens',
+        'scattered clouds': 'Nuvens dispersas',
+        'broken clouds': 'Nuvens quebradas',
+        'shower rain': 'Aguaceiro',
+        'rain': 'Chuva',
+        'thunderstorm': 'Tempestade',
+        'snow': 'Neve',
+        'mist': 'Névoa'
+      };
+
+    const convertedData = {
+        date: new Date(weather.date).toLocaleDateString('pt-BR'),
+        temp: (weather.temp - 273.15).toFixed(2),
+        tempMax: (weather.tempMax - 273.15).toFixed(2),
+        tempMin: (weather.tempMin - 273.15).toFixed(2),
+        description: weatherDescriptions[weather.description] || weather.description,
+    };
+
+    return (
+        <div className="weather-info">
+            <header className="weather-header">
+                <h2>{weather.city}</h2>
+            </header>
+            <main className="weather-content">
+                <p>Data: {convertedData.date}</p>
+                <p>Temperatura: {convertedData.temp}°C</p>
+                <p>Temp Max: {convertedData.tempMax}°C</p>
+                <p>Temp Min: {convertedData.tempMin}°C</p>
+                <p>Descrição: {convertedData.description}</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.icon}.png`} alt={weather.description} />
+            </main>
+        </div>
+    );
 };
 
 export default WeatherInfo;
