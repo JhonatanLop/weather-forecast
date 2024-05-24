3c8a44e35466bfe1b5061a0f79c713f934bce30d
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-24 11:27:34 -0300

    chore: Add CSS file for SavedCities component and update component code to work like a placeholder when citiesProps is []

--- weather-forecast/src/components/SavedCities/SavedCities.tsx
+++ weather-forecast/src/components/SavedCities/SavedCities.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import './savedCities.css';
 
 interface SavedCitiesProps {
-  cities: string[];
-  onSelectCity: (city: string) => void;
+    cities: string[];
+    onSelectCity: (city: string) => void;
 }
 
+
 const SavedCities: React.FC<SavedCitiesProps> = ({ cities, onSelectCity }) => {
-  return (
-    <div>
-      <select onChange={(e) => onSelectCity(e.target.value)}>
-        <option value="">Select a saved city</option>
-        {cities.map((city, index) => (
-          <option key={index} value={city}>
-            {city}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+    if (cities.length === 0) {
+        return (
+            <div>
+                <select onChange={(e) => onSelectCity(e.target.value)} className="select-list">
+                    <option value="" >Histórico de busca</option>
+                </select>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <select onChange={(e) => onSelectCity(e.target.value)} className="select-list">
+                {cities.map((city, index) => (
+                    <option key={index} value={city}>
+                        {city}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 };
 
 export default SavedCities;
